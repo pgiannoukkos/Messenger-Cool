da20cad78949347fb62aac7a4bbc8eb822c6b048
Panayiotis Yiannoukkos <p.yiannoukkos@outlook.com>
2020-01-04 13:10:04 +0200

    #11 - Checks for registration

--- src/main/java/www/project/dao/UserDAO.java
+++ src/main/java/www/project/dao/UserDAO.java
@@ -5,7 +5,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.loader.custom.sql.SQLCustomQuery;
 import org.hibernate.query.NativeQuery;
 import www.project.bean.User;
 
